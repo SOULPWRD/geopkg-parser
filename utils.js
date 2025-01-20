eeddfa4d8109f396114f2ea5c73d0a8aca3f1e3d
Martin Pravda <martin.pravda@gmail.com>
2025-01-20 07:45:00 +0100

    decoder

--- utils.js
+++ utils.js
@@ -1,12 +1,20 @@
 // Martin Pravda
 // utils.js
+// A simple utility functions
 
 /*jslint browser, node */
+
+const utf8_decoder = new TextDecoder("utf-8");
 
 function make_empty_list(length) {
     return [...new Array(length)];
 }
 
+function decode_text(byte_array) {
+    return utf8_decoder.decode(byte_array);
+}
+
 export default Object.freeze({
+    decode_text,
     make_empty_list
 });
