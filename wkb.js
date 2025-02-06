63730e36aa6f880cac4b6f718528826fd32c4aa0
Martin Pravda <martin.pravda@gmail.com>
2025-02-06 16:23:35 +0100

    fix & refactor

--- wkb.js
+++ wkb.js
@@ -1,9 +1,11 @@
 // wkb.js
 // Martin Pravda
+// A simple WKB reader
 
 /*jslint browser, node, bitwise, for */
 
 import utils from "./utils.js";
+import binary from "./binary.js";
 
 function encode_hex_string(buffer) {
     const view = new Uint8Array(buffer);
@@ -29,150 +31,161 @@ function decode_hex_string(text) {
     return view;
 }
 
-function make_instance(view) {
-    let offset_pointer = 0;
-    let is_little_endian = false
-
-    function read_uint8() {
-        const data = view.getUint8(offset_pointer);
-        offset_pointer += 1;
-        return data;
-    }
-
-    function read_uint32() {
-        const data = view.getUint32(offset_pointer, is_little_endian);
-        offset_pointer += 4;
-        return data;
-    }
-
-    function read_double() {
-        const data = view.getFloat64(offset_pointer, is_little_endian);
-        offset_pointer += 8;
-        return data;
-    }
-
-// header data
-
-    is_little_endian = read_uint8() > 0;
-    const wkb_type = read_uint32();
-    const wkb_type_thousandth = Math.floor(
-        (wkb_type & 0x0fffffff) / 1000
-    );
-    const has_z = (
-        Boolean(wkb_type & 0x80000000)
-        || wkb_type_thousandth === 1
-        || wkb_type_thousandth === 3
-    );
-    const has_m = (
-        Boolean(wkb_type & 0x40000000)
-        || wkb_type_thousandth === 2
-        || wkb_type_thousandth === 3
+function calculate_geometry_props(wkb_type) {
+    const is_ewkb = (
+        wkb_type & 0x20000000
+        || wkb_type & 0x40000000
+        || wkb_type & 0x80000000
     );
 
-    const srid = (
-        Boolean(wkb_type & 0x20000000)
-        ? read_uint32()
-        : undefined
-    );
-
-    const type_id = (wkb_type & 0x0fffffff) % 1000;
-
-    function read_point() {
-        const coords = [];
-    
-    // read x and y coord
-    
-        coords.push(read_double());
-        coords.push(read_double());
-    
-    // read z and m coord if exist
-    
-        if (has_z) {
-            coords.push(read_double());
+    if (Boolean(is_ewkb) === false) {
+        if (wkb_type >= 1000 && wkb_type < 2000) {
+            return Object.freeze({
+                geometry_type: wkb_type - 100,
+                has_m: false,
+                has_z: true
+            });
         }
-    
-        if (has_m) {
-            coords.push(read_double());
+
+        if (wkb_type >= 2000 && wkb_type < 3000) {
+            return Object.freeze({
+                geometry_type: wkb_type - 2000,
+                has_m: true,
+                has_z: false
+            });
         }
-    
-        return coords;
-    }
 
-    function read_linestrig() {
-        const num_points = read_uint32();
-        const coords = [];
-        for (let i = 0; i < num_points; i++) {
-            coords.push(read_point());
-            console.log({coords});
+        if (wkb_type >= 3000 && wkb_type < 4000) {
+            return Object.freeze({
+                geometry_type: wkb_type - 3000,
+                has_m: true,
+                has_z: true
+            });
         }
-        return coords;
-    }
 
-    function read_polygon() {
-        const num_rings = read_uint32();
-        const rings = [];
-        for (let i = 0; i < num_rings; i++) {
-            rings.push([read_linestrig()]);
-            console.log({rings});
-        }
-        return rings;
-    }
-
-    function read_multi_polygon() {
-        const num_polygons = read_uint32();
-        const rings = [];
-        for (let i = 0; i < num_polygons; i++) {
-            rings.push([read_polygon()]);
-            console.log({rings});
-        }
-        return rings;
-    }
-
-    const readers_map = {
-        "1": read_point,
-        "2": read_linestrig,
-        "3": read_polygon,
-        "6": read_multi_polygon
-    };
-
-    function header() {
         return Object.freeze({
-            is_little_endian,
-            type_id,
-            has_z,
-            has_m,
-            srid
+            geometry_type: wkb_type,
+            has_m: false,
+            has_z: false
         });
     }
 
-    function geometry() {
-        return readers_map[type_id]();
+    let has_z = false;
+    let has_m = false;
+
+    if (Boolean(wkb_type & 0x80000000)) {
+        has_z = true;
+    }
+
+    if (Boolean(wkb_type & 0x40000000)) {
+        has_m = true;
     }
 
     return Object.freeze({
-        geometry,
-        header
+        geometry_type: wkb_type & 0xF,
+        has_m,
+        has_z
     });
 }
 
-import wkb_mock from "./mocks/wkb.js";
-const wkb = make_instance(decode_hex_string(wkb_mock.hex_string));
-const wkb_multipolygon = make_instance(decode_hex_string(wkb_mock.multipolygon));
-const wkb_point = make_instance(decode_hex_string(wkb_mock.point));
-const header = wkb.header();
-try {
-    const geom = wkb.geometry();
-    const geom_multipolygon = wkb_multipolygon.geometry();
-    const geom_point = wkb_point.geometry();
-    const geom_point_header = wkb_point.header();
-} catch (err) {
-    console.log(err);
+// header data
+function read_header(binary_reader) {
+    binary_reader.set_little_endian(
+        binary_reader.read_uint8() > 0
+    );
+
+    const wkb_type = binary_reader.read_uint32();
+    const srid = (
+        Boolean(wkb_type & 0x20000000)
+        ? binary_reader.read_uint32()
+        : undefined
+    );
+
+    const {
+        geometry_type,
+        has_m,
+        has_z
+    } = calculate_geometry_props(wkb_type);
+
+    return Object.freeze({
+        geometry_type,
+        has_m,
+        has_z,
+        srid,
+        wkb_type
+    });
 }
 
+function read_point(binary_reader, header) {
+    const coords = [];
+
+// read x and y coord
+
+    coords.push(binary_reader.read_float64());
+    coords.push(binary_reader.read_float64());
+
+// read z and m coord if exist
+
+    if (header.has_z) {
+        coords.push(binary_reader.read_float64());
+    }
+
+    if (header.has_m) {
+        coords.push(binary_reader.read_float64());
+    }
+
+    return coords;
+}
+
+function read_linestrig(binary_reader, header) {
+    const num_points = binary_reader.read_uint32();
+    return utils.make_empty_list(num_points).reduce(function (coords) {
+        coords.push(read_point(binary_reader, header));
+        return coords;
+    }, []);
+}
+
+function read_polygon(binary_reader, header) {
+    const num_rings = binary_reader.read_uint32();
+    return utils.make_empty_list(num_rings).reduce(function (coords) {
+        coords.push(read_linestrig(binary_reader, header));
+        return coords;
+    }, []);
+}
+
+function read_wkb_collection(read_fn) {
+    return function (binary_reader) {
+        const num = binary_reader.read_uint32();
+        return utils.make_empty_list(num).reduce(function (coords) {
+            coords.push(read_fn(binary_reader, read_header(binary_reader)));
+            return coords;
+        }, []);
+    };
+}
+
+const readers_map = {
+    "1": read_point,
+    "2": read_linestrig,
+    "3": read_polygon,
+    "6": read_wkb_collection(read_polygon)
+};
+
+function read_payload(view) {
+    const reader = binary.reader(view);
+    const header = read_header(reader);
+    return readers_map[header.geometry_type](reader, header);
+}
+
+//demo import wkb_mock from "./mocks/wkb.js";
+//demo const wkb_point = read_payload(decode_hex_string(wkb_mock.point));
+//demo const wkb_polygon = read_payload(decode_hex_string(wkb_mock.polygon));
+//demo const wkb_multipolygon = read_payload(
+//demo     decode_hex_string(wkb_mock.multipolygon
+//demo ));
 
 
 export default Object.freeze({
     decode_hex_string,
     encode_hex_string,
-    make_instance
+    read: read_payload
 });
